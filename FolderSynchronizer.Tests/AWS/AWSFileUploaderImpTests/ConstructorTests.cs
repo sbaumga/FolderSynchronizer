fa8c0af6940d5d669e641e79991f8a558748a849
Shayne Baumgartner <shayneb34@gmail.com>
2021-11-29 14:34:43 -0700

    Added file uploader constructor tests.

---
+++ FolderSynchronizer.Tests/AWS/AWSFileUploaderImpTests/ConstructorTests.cs
@@ -0,0 +1,66 @@
+﻿using FolderSynchronizer.Abstractions;
+using FolderSynchronizer.AWS.Abstractions;
+using FolderSynchronizer.AWS.Implementations;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+using System;
+
+namespace FolderSynchronizer.Tests.AWS.AWSFileUploaderImpTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        public enum NullConstructorArg 
+        { 
+            Logger,
+            PathManger,
+            ActionTaker,
+            FileLister,
+            ConfigData
+        }
+
+        [Test]
+        [TestCase(NullConstructorArg.Logger)]
+        [TestCase(NullConstructorArg.PathManger)]
+        [TestCase(NullConstructorArg.ActionTaker)]
+        [TestCase(NullConstructorArg.FileLister)]
+        [TestCase(NullConstructorArg.ConfigData)]
+        public void NullLoggerTest(NullConstructorArg nullArg)
+        {
+            ILogger<AWSFileUploaderImp> logger = nullArg == NullConstructorArg.Logger ? null : GetLogger();
+            IAWSPathManager pathManger = nullArg == NullConstructorArg.PathManger ? null : GetPathManger();
+            IAWSActionTaker actionTaker = nullArg == NullConstructorArg.ActionTaker ? null : GetActionTaker();
+            ILocalFileLister fileLister = nullArg == NullConstructorArg.FileLister ? null : GetLocalFileLister();
+
+            ConfigData configData = nullArg == NullConstructorArg.ConfigData ? null : GetConfigData();
+
+            Should.Throw<ArgumentNullException>(() => new AWSFileUploaderImp(logger, pathManger, actionTaker, fileLister, configData));
+        }
+
+        private ILogger<AWSFileUploaderImp> GetLogger()
+        {
+            return new Mock<ILogger<AWSFileUploaderImp>>(MockBehavior.Strict).Object;
+        }
+
+        private IAWSPathManager GetPathManger()
+        {
+            return new Mock<IAWSPathManager>(MockBehavior.Strict).Object;
+        }
+
+        private IAWSActionTaker GetActionTaker()
+        {
+            return new Mock<IAWSActionTaker>(MockBehavior.Strict).Object;
+        }
+
+        private ILocalFileLister GetLocalFileLister()
+        {
+            return new Mock<ILocalFileLister>(MockBehavior.Strict).Object;
+        }
+
+        private ConfigData GetConfigData()
+        {
+            return new ConfigData { BucketName = "TestBucket" };
+        }
+    }
+}
