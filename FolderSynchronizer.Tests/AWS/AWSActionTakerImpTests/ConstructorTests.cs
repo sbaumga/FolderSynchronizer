5a67808bae6b6ddbde61deb5b66bc2155ef1458a
Shayne Baumgartner <shayneb34@gmail.com>
2021-11-29 14:18:10 -0700

    Added constructor tests for action taker and path manager

---
+++ FolderSynchronizer.Tests/AWS/AWSActionTakerImpTests/ConstructorTests.cs
@@ -0,0 +1,17 @@
+﻿using FolderSynchronizer.AWS.Implementations;
+using NUnit.Framework;
+using Shouldly;
+using System;
+
+namespace FolderSynchronizer.Tests.AWS.AWSActionTakerImpTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void NullConfigData()
+        {
+            Should.Throw<ArgumentNullException>(() => new AWSActionTakerImp(null));
+        }
+    }
+}
---
+++ FolderSynchronizer.Tests/AWS/AWSPathManagerImpTests/ConstructorTests.cs
@@ -0,0 +1,17 @@
+﻿using FolderSynchronizer.AWS.Implementations;
+using NUnit.Framework;
+using Shouldly;
+using System;
+
+namespace FolderSynchronizer.Tests.AWS.AWSPathManagerImpTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void NullConfigData()
+        {
+            Should.Throw<ArgumentNullException>(() => new AWSPathManagerImp(null));
+        }
+    }
+}
