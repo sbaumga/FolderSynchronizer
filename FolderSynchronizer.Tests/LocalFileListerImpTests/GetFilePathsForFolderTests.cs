c7c1e2dcbf63a5aaabe3033f7e7be5c5c243e211
Shayne Baumgartner <shayneb34@gmail.com>
2021-12-08 19:57:10 -0700

    Added tests for LocalFileListerImp.GetFilePathsForFolderTests

---
+++ FolderSynchronizer.Tests/LocalFileListerImpTests/GetFilePathsForFolderTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FolderSynchronizer.Tests.LocalFileListerImpTests
+{
+    public class GetFilePathsForFolderTests : LocalFileListerImpTestBase
+    {
+        [Test]
+        public void FolderDoesNotExistTest()
+        {
+            Should.Throw<DirectoryNotFoundException>(() => FileLister.GetFilePathsForFolder("Garbage"));
+        }
+
+        [Test]
+        public void EmptyFolderTest()
+        {
+            var result = RunGetFilePathsForFolderOnTestFolder();
+
+            result.ShouldBeEmpty();
+        }
+
+        private IEnumerable<string> RunGetFilePathsForFolderOnTestFolder()
+        {
+            var folderPath = GetTestFolderPath();
+            var result = FileLister.GetFilePathsForFolder(folderPath);
+
+            return result;
+        }
+
+        [Test]
+        public void SingleFileTest()
+        {
+            DoFileTest("TestFile.txt");
+        }
+
+        private void DoFileTest(params string[] fileNames)
+        {
+            foreach (var file in fileNames)
+            {
+                CreateFileInTestFolder(file);
+            }
+
+            var expectedFilePaths = fileNames.Select(n => GetFullExpectedPathForFile(n));
+
+            var result = RunGetFilePathsForFolderOnTestFolder().ToList();
+
+            result.Count().ShouldBe(fileNames.Length);
+
+            foreach (var file in expectedFilePaths)
+            {
+                result.ShouldContain(file);
+            }
+        }
+
+        private void CreateFileInTestFolder(string fileName)
+        {
+            var filePath = GetFullExpectedPathForFile(fileName);
+            using var _ = File.Create(filePath);
+        }
+
+        private string GetFullExpectedPathForFile(string fileName)
+        {
+            var folderPath = GetTestFolderPath();
+            var filePath = Path.Combine(folderPath, fileName);
+            return filePath;
+        }
+
+        [Test]
+        public void ThreeFileTest()
+        {
+            DoFileTest("TestFile1.txt", "TestFile2.mp3", "TestFile3.jpg");
+        }
+    }
+}
---
+++ FolderSynchronizer.Tests/LocalFileListerImpTests/LocalFileListerImpTestBase.cs
@@ -0,0 +1,58 @@
+﻿using FolderSynchronizer.Implementations;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace FolderSynchronizer.Tests.LocalFileListerImpTests
+{
+    [TestFixture]
+    public abstract class LocalFileListerImpTestBase
+    {
+        protected LocalFileListerImp FileLister { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            CreateBaseTestFolder();
+
+            FileLister = new LocalFileListerImp();
+        }
+
+        private void CreateBaseTestFolder()
+        {
+            var testFolderPath = GetTestFolderPath();
+
+            if (Directory.Exists(testFolderPath))
+            {
+                DeleteTestFolder();
+            }
+
+            Directory.CreateDirectory(testFolderPath);
+        }
+
+        protected string GetTestFolderPath()
+        {
+            var currentFolder = GetCurrentFolderPath();
+            return Path.Combine(currentFolder, TestFolderName);
+        }
+
+        private string GetCurrentFolderPath()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+
+        protected string TestFolderName = "LocalFileListerImpTest Files";
+
+        [TearDown]
+        public void TearDown()
+        {
+            DeleteTestFolder();
+        }
+
+        private void DeleteTestFolder()
+        {
+            var testFolderPath = GetTestFolderPath();
+            Directory.Delete(testFolderPath, true);
+        }
+    }
+}
