19e66af889989cf344f478d8e5db370a4f72d074
Shayne Baumgartner <shayneb34@gmail.com>
2021-12-13 19:57:20 -0700

    Added ServiceRegistrationTests

---
+++ FolderSynchronizer.Tests/TypeMappingTests/ServiceRegistrationTests.cs
@@ -0,0 +1,19 @@
+﻿using FolderSynchronizer.TypeMapping;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Shouldly;
+
+namespace FolderSynchronizer.Tests.TypeMappingTests
+{
+    [TestFixture]
+    public class ServiceRegistrationTests
+    {
+        [Test]
+        public void SmokeTest()
+        { 
+            var serviceCollection = new ServiceCollection();
+
+            Should.NotThrow(() => ServiceRegistation.Register(serviceCollection));
+        }
+    }
+}
