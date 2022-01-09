130059709c4addc71d0d9ac39ddddc9c26e8ae4d
Shayne Baumgartner <shayneb34@gmail.com>
2022-01-08 22:04:30 -0700

    Improved typemapping tests

--- FolderSynchronizer.Tests/TypeMappingTests/ServiceRegistrationTests.cs
+++ FolderSynchronizer.Tests/TypeMappingTests/ServiceRegistrationTests.cs
@@ -2,6 +2,10 @@
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace FolderSynchronizer.Tests.TypeMappingTests
 {
@@ -9,11 +13,37 @@ namespace FolderSynchronizer.Tests.TypeMappingTests
     public class ServiceRegistrationTests
     {
         [Test]
-        public void SmokeTest()
-        { 
+        public void RegistrationTest()
+        {
             var serviceCollection = new ServiceCollection();
 
-            Should.NotThrow(() => ServiceRegistation.Register(serviceCollection));
+            ServiceRegistation.Register(serviceCollection);
+
+            VerifyAllImplemenationsAreRegistered(serviceCollection);
+        }
+
+        private void VerifyAllImplemenationsAreRegistered(ServiceCollection serviceCollection)
+        {
+            var implementationTypes = GetImplementationTypes();
+
+            foreach(var impType in implementationTypes)
+            {
+                VerifyImplementationTypeIsRegistered(serviceCollection, impType);
+            }
+        }
+
+        private IEnumerable<Type> GetImplementationTypes()
+        {
+            var assembly = Assembly.GetAssembly(typeof(ServiceRegistation));
+            var types = assembly.GetTypes();
+            var implementationTypes = types.Where(t => t.Namespace != null && t.Namespace.Contains("Implementations") && !t.IsAbstract && t.IsPublic && t.IsClass);
+
+            return implementationTypes;
+        }
+
+        private void VerifyImplementationTypeIsRegistered(ServiceCollection serviceCollection, Type type)
+        {
+            serviceCollection.ShouldContain(d => d.ImplementationType == type);
         }
     }
 }
